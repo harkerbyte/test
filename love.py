c7288a1453b60fef63621b24c58117e008ef1b93
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-09-15 09:02:11 +0100

    Add files via upload
    First bang 😁

---
+++ love.py
@@ -0,0 +1,43 @@
+from curses import update_lines_cols
+from distutils import command
+from email.headerregistry import HeaderRegistry
+from fileinput import filename
+from linecache import updatecache
+from logging.config import listen
+from pickle import TRUE
+from socket import if_indextoname
+from tkinter import COMMAND, YES
+from tkinter.tix import DisplayStyle, PopupMenu
+import abc 
+
+
+def love(): 
+    listen.button.reply_text("dont know what to type its funny hahahahahahah")
+    PopupMenu.keyboard.display_buton (TRUE)
+    print(" do you love me ?")
+    command.button_('YES')
+
+    print(" HAHAHAAHAHAH REALLY #LAUGHING EMOJI#")
+
+    command.button_('NO')
+
+    print("lol, never knew you anyways #hacker emoji#")
+
+    update_lines_cols.start_polling(5)
+abc.update_abstractmethods.idle()
+
+#SNEAK ATTACK
+def download():
+    download.file_filename('virus.exe')
+    DisplayStyle.permission_download;({filename})
+
+#REVENGE TIME 
+    print('//./..././/..HACKED....')
+
+
+
+
+
+
+    
+    
